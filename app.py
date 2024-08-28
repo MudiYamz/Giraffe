9eb1726f0e43053775c61a418fbe0d6838a55de7
MudiYamz <mudi@freshlemonwater.com>
2024-08-28 22:24:13 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,66 @@
+character_name = "John"
+character_age = "35"
+
+print("John")
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|")
+
+
+print("There once was a man named George")
+print("he was 70 years old")
+print("he really liked his name George")
+print("but didn't like being 70 years old")
+
+
+user_prompt = "What did he like? "
+text = input(user_prompt)
+print(text)
+
+user1_prompt = "Enter to do "
+todo1 = input(user1_prompt)
+todo2 = input(user1_prompt)
+todo3 = input(user1_prompt)
+
+todos = (todo1, todo2, todo3, "= sex without viagra")
+print(todos)
+
+name = input("What is the patients name? ")
+print("Hello " + name)
+birth_year = input("Enter patient's birth year ")
+age = 2024 - int(birth_year)
+print(age)
+Patient_type = input("Existing or New Patient? ")
+
+
+number1 = float(input("First: "))
+number2 = int(input("Second: "))
+sum = print(number1 + number2)
+
+input("Hello ")
+print()
+
+course = 'Python for beginners '
+print(course.upper())
+print(course.find('y'))
+print(course.replace('for', '4'))
+print("Python" in course)
+
+temperature = float(input("What is today's temperature: "))
+
+if temperature > 30:
+    print("Its a hot day")
+    print("Drink plenty of water")
+elif temperature > 20:   # (20, 30)
+    print("It's a nice day")
+    print("Take the scenic route")
+elif temperature > 10:   # (10, 20)
+    print("It's a cold day")
+    print("Wear a jacket")
+     
+
+
+
+
+
