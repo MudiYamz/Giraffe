11f23d5b68eda5e30135fbbf9d7d9221432839b2
MudiYamz <175753786+MudiYamz@users.noreply.github.com>
2025-03-27 15:54:18 +0000

    An update based on current level of training

--- app.py
+++ app.py
@@ -248,3 +248,17 @@ for new_user in new_users:
         print("\n", new_user + " already exist; Enter new username." )
     else:
         print(new_user + " :this username is available.")
+
+ordinal_numbers = list(range(1, 10))
+
+for ordinal_number in ordinal_numbers:
+    if ordinal_number == 1:
+        print("1st")
+    elif ordinal_number == 2:
+        print("2nd")
+    elif ordinal_number == 3:
+        print("3rd")
+    else:
+        print(str(ordinal_number) + "th")
+
+
