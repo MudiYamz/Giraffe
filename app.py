20b787cdfd504af11599dd538129d34f7dfa9406
MudiYamz <175753786+MudiYamz@users.noreply.github.com>
2025-03-27 15:15:03 +0000

    An update based on current level of training

--- app.py
+++ app.py
@@ -211,4 +211,15 @@ user = 'Chanel'
 if user not in banned_users:  # Checking whether a value is not in a list, so user sees a message.
     print("\n", user.title() + ",you can post a response if you wish.")
 
+available_toppings = ["mushrooms", "olives", "green peppers", "pepperoni", "pineapple", "extra cheese"]
+# Using multiple lists.
+requested_toppings = ["mushrooms", "french fries", "extra cheese"]
+
+for requested_topping in requested_toppings:
+    if requested_topping in available_toppings:
+        print("\nAdding " + requested_topping + ".")
+    else:
+        print("Sorry, we don't have " + requested_topping + ".")
+
+print("\nFinished making your Pizza!")
 
